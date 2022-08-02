279897b67b988f9ad4818c60c58f95ac4ff0be39
Camila Canavesio <canavesiocamila@gmail.com>
2022-08-01 23:11:11 -0300

    algoritmoconciclo

---
+++ app.js
@@ -0,0 +1,10 @@
+// Creación de algoritmo utilizando un ciclo
+
+
+let listaDeAlumnos = prompt("Ingrese el nombre del Alumno para tomar asistencia o ESCAPE para finalizar")
+while(listaDeAlumnos !="ESCAPE"){
+    console.log(" Presente " + listaDeAlumnos)
+    listaDeAlumnos = prompt("Ingrese el nombre del Alumno para tomar asistencia o ESCAPE para finalizar")
+}
+
+ 
